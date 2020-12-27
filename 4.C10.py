bc9d9873226172dfc2c8842654d81262ff59d3e8
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-12-27 22:28:19 +0700

    Create 4.C10.py

---
+++ 4.C10.py
@@ -0,0 +1,28 @@
+#4
+mencariNim = input('Inputkan NIM yang dicari :')
+
+file = open('textNo2.txt', 'r')
+
+y = file.readlines()
+
+for x in range(len(y)) :
+    
+    if(mencariNim in y[x]) :
+        status = 'ada'
+        break
+
+    else :
+        status = 'tidak ada'
+        continue
+
+if(status == 'ada') :
+    
+        z = y[x].split('|')
+        
+        print('\nData Mahasiswa')
+        print('NIM    :', z[0])
+        print('Nama   :', z[1])
+        print('Alamat :', z[2])
+
+else :
+    print('Data mahasiswa tidak ditemukan')
