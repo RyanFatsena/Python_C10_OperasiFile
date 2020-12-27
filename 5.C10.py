79bf6b39be369ff16a1bb20bd74fe29821fa57fe
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-12-27 22:28:17 +0700

    Create 5.C10.py

---
+++ 5.C10.py
@@ -0,0 +1,27 @@
+#5
+file = open('textNo5.txt', 'r')
+
+result = []
+for x in file.readlines() :
+    if('\n' in x) :
+        
+        replaced = x.replace('\n','')
+        splitted = replaced.split('|')
+        valueList = int(splitted[0]) + int(splitted[1])
+        result.append(valueList)
+        
+    else :
+        
+        splitted = x.split('|')
+        valueList = int(splitted[0]) + int(splitted[1])
+        result.append(valueList)
+
+file.close()
+
+output = open('hasilTextNo5.txt', 'w')
+
+for x in range(len(result)) :
+    output.write(str(result[x]) + '\n')
+
+output.close()
+print('Silakan lihat hasil penjumlahan dalam file hasilTextNo5.txt')
